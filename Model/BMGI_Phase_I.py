9816e2c576867096ef07cdd4af928d998d89bb12
maburidi@gmail.com <maburidi@gmail.com>
2019-08-23 19:14:13 -0400

    done almost

--- Model/BMGI_Phase_I.py
+++ Model/BMGI_Phase_I.py
@@ -5,7 +5,7 @@ Random Forest is used here to score every gene for classification power using it
 isoforms expression values as features.
 
 Required Inputs:
-1- Annotated gene sets. Each gene is represented by an Ensemble ID (e.g. ). txt file
+1- Annotated gene sets. Each gene is represented by an Ensemble ID (e.g. ENSG00000159251). txt file
 2- TEM: sigle-cell processed transcriptomic expression data (rows are the samples (i.g. cells), columns are the transcripts). csv file
 3- GEM: sigle-cell processed gene expression data (rows are the samples (i.g. cells), columns are the genes). csv file
 4- Sample labels (samples (i.g. cells) belongs to classes, each samples should have a label). csv file
@@ -17,7 +17,8 @@ Not required inputs:
 8- Number of folds for cross validation (defult =10). 
 
 Outputs:
-1- 
+json file contains the Annotated gene sets, each set has  genes that have significant classification power to random gene. 
+The jason file contains all of the statistics of each gene.    
 
 
 
@@ -29,7 +30,8 @@ python Model/BMGI_Phase_I.py \
     --GEM_dataset  /home/maburid/AMIA_project/Gene_stem_cell.csv \
     --labels       /home/maburid/AMIA_project/labels.csv \
     --num_trees    50 \
-    --cv           5
+    --cv           5 \
+    --outfile /home/maburid/AMIA_project/
 
 """
 
@@ -42,6 +44,7 @@ import sklearn.ensemble
 import sklearn.mixture
 import scipy
 import argparse
+import copy
 
 import random
 import sys
@@ -105,8 +108,9 @@ if __name__ == "__main__":
     parser.add_argument("--num_trees", help="number of trees in random forest", type=int, default=100)
     parser.add_argument("--cv", help="number of folds for k-fold cross validation", type=int, default=10)
     parser.add_argument("--p_threshold", help="maximum p-value required for a gene to be selected", type=float, default=0.001)
-    parser.add_argument("--percent_threshold", help="minimum percentage of filtered genes in a set to the origional genes, otherwise, if less \
-    than this threshould, dont count the set as a significant", type=float, default=0.6)
+    parser.add_argument("--percent_threshold", help="minimum percentage of filtered genes in a set to the origional genes, otherwise, if less than this threshould, dont count the set as a significant", type=float, default=0.6)
+    parser.add_argument("--outfile", help="output dir to save results",required=True )
+
     
     args = parser.parse_args()
 
@@ -125,10 +129,10 @@ if __name__ == "__main__":
     
     # load annotated gene sets
     with open(filename_gene_sets) as f:
-    gene_sets = []
-    for line in f:
-        line = line.split()  
-        gene_sets.append(line)
+        gene_sets = []
+        for line in f:
+            line = line.split()  
+            gene_sets.append(line)
     
     
     #=======================================
@@ -198,14 +202,145 @@ if __name__ == "__main__":
     TX_SET= set([int(Trans_names[tx_cnt][4:]) for tx_cnt in range(len(Trans_names))])    # set of all transcripts IDs as integers 
     path_to_ref='/home/maburid/BioinfAlgoProject_spring_2019/Homo_sapiens.GRCh38.rel79.cdna.all.utr_mod.fa'    # change this 
     [TX_to_ENSG,ENSG_isoforms]=readENS_ids(path_to_ref)
+    
+    rand_score = []
+    
+    # get accuracy for random gene
+    for u in range(0,100):
+        gene = random.choice(list(ENSG_isoforms.keys()))    
+        isoforms=ENSG_isoforms[str(gene)]
+        ## Data for the model   
+        X = selected_data[isoforms]
+        Y = list(ss["Class_label"])  
+        clf = sklearn.ensemble.RandomForestClassifier(n_estimators=args.num_trees)
+        X_train, X_test, y_train, y_test = sklearn.model_selection.train_test_split(X, Y, test_size=0.3)       
+        clf = copy.deepcopy(clf)                                                                               
+        clf.fit(X_train, y_train)                                                                         
+        scores = clf.score(X_test, y_test)                                                               
+        rand_score.append(scores)                      
+                                                                                                              
+    rand_mean = np.mean(rand_score)                                                                 
+    rand_std = np.std(rand_score)     
+    rand_max = np.max(rand_score)    
+    rand_min = np.min(rand_score)
+    
+    ## Run the Model: 
+    
+    thresh_p_value = args.p_threshold               
+    genes = list(ENSG_isoforms.keys())    # 37940 gene  and 172941 transcripts 
+                                                                            
+    all_infor =[]
+    set_name = gene_sets[u][0]
+    gene_accuracy ={}  
+    two_max_isoforms ={}                            
+    C_V = args.cv                  # num of folds for cv 
+
+    for i in range(0,len(genes)): 
+        #if i == 50:              
+        #    break 
+        #if str(genes[i]) in list(ENSG_isoforms.keys()):
+        #================================ apply RF on gene transcripts 
+        isoforms=ENSG_isoforms[str(genes[i])]
+    
+        ## Data for the model 
+        X = selected_data[isoforms]
+        Y = list(ss["Class_label"])
+            #print("Data_shape (num_sample, num_isoforms):",X.shape)
+            #print("Number_of_samples: ",len(Y))    
+        clf = sklearn.ensemble.RandomForestClassifier(n_estimators=args.num_trees)
+        clf.fit(X, Y)
+        scores = sklearn.model_selection.cross_val_score(clf, X, y=Y, cv=C_V)
+            #y_pred_prob = sklearn.model_selection.cross_val_predict(clf, X, y=Y, cv=5 )
+        isoform_score = list(clf.feature_importances_ )
+        
+        scores_for_plot = []
+        sel_mean = np.mean(scores)
+        sel_std = np.std(scores)
+        sel_max = np.max(scores)
+        sel_min = np.min(scores)
+    
+        scores_for_plot.append(sel_mean)
+        scores_for_plot.append(sel_std)
+        scores_for_plot.append(sel_max)
+        scores_for_plot.append(sel_min)
+        
+        #============================ get p_value 
+        t,p_value = scipy.stats.ttest_ind_from_stats(rand_mean, rand_std, 100, sel_mean, sel_std, C_V, equal_var=False)
+        scores_for_plot.append(p_value)
+        gene_accuracy[str(genes[i])] = scores_for_plot
+                    
+        #============================
+            # this is to get the maximum accurate isoforms. 
+        max_isofomes =[] 
+        index, value = max(enumerate(isoform_score), key=operator.itemgetter(1))
+        max_isofomes.append(index)
+        isoform_score.remove(value)
+        if len(isoform_score) > 0:
+            index, value = max(enumerate(isoform_score), key=operator.itemgetter(1))
+            max_isofomes.append(index)
+        max_isofomes = [ isoforms[i] for i in max_isofomes ]
+        two_max_isoforms[genes[i]] = max_isofomes
+        #=============================
+        print(i)
+        i = i+1
+    
+    
+    informative_genes ={} 
+    for ggene in gene_accuracy:
+        if gene_accuracy[ggene][0] > rand_mean and gene_accuracy[ggene][4] < thresh_p_value :
+            informative_genes[ggene] =gene_accuracy[ggene]
+    print(informative_genes)
+        
+    #all_infor.append(informative_genes) 
+    #all_infor.append(len(list(informative_genes.keys()))/len(genes))                  # save the percent of the informative genes
+    #sig_sets[set_name] = all_infor
+    two_infor_isoforms ={}
+
+    for two_iso in list(two_max_isoforms.keys()):
+        if two_iso in list(two_infor_isoforms.keys()):
+            continue
+        two_infor_isoforms[two_iso] = two_max_isoforms[two_iso]
+
+    #=============================================================
+    # Group
+    sig_sets = {}
+
+    for u in range(0,len(gene_sets)):                           
+        #if u == 16:                            
+        #    break                                   
+    
+        all_infor =[]             
+        set_name = gene_sets[u][0]          
+        genes = symb_to_id(gene_sets[u][2:])   
+        #print("===================================================================")
+        #print("Set Name: ",set_name)
+        print("Set Number: ", u)
+        #print("Number of Genes:", len(genes))
+        get_sig_genes= {}
+        combine_prec =[] 
+        for i in range(len(genes)):
+            if genes[i] in list(informative_genes.keys()):
+                get_sig_genes[str(genes[i])] = informative_genes[str(genes[i])]
+        combine_prec.append(get_sig_genes)
+        if len(genes) >0:
+            combine_prec.append(len(list(get_sig_genes.keys()))/len(genes))                
+        else: 
+            combine_prec.append(int(0))    
+        sig_sets[str(set_name)] = combine_prec                              
+    
+                                                                                        
+    json2 = json.dumps(sig_sets)                                         
+    ff = open( args.outfile + "BMGI_Phase_I_results.json","w")             
+    ff.write(json2)                                                                                                       
+    ff.close()  
+    
+   
+    
+
+    
+    
+    
+
+    
 
 
-    
-    
-    
-    
-    
-    
-    
-
-    
